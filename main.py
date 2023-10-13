76a05d25bd065ddd28a2c4dd88e6dbeca3fafb00
dongzen <krtdj@naver.com>
2023-10-13 15:16:05 +0900

    11

--- main.py
+++ main.py
@@ -38,12 +38,22 @@ def daily():
     except Exception as e:
         logger.warning('Daily CDU common Fail' +str(e))
 
+def kafka():
+    try:
+        now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        print('\rdaily', end ="")
+        print(now)
+        Kafka_Con()
+        logger.info('Kafka Succeeded!')
+    except Exception as e:
+        logger.warning('Kafka Fail' +str(e))
+
 
 def main():
     try :
         CTMPI()
         CTDPI()
-        # Kafka_Con()
+        Kafka_Con()
         logger.info('Tanium Minutely common 성공')
         print('Tanium Minutely common 성공')
 
@@ -78,6 +88,7 @@ def main():
     #test용
     sched.add_job(minutely, 'cron', hour='0-23', minute='0', second='10', misfire_grace_time=None)  # seconds='3'
     sched.add_job(daily, 'cron', hour='0-23', minute='10',  second='20' , misfire_grace_time=None)
+    sched.add_job(kafka, 'cron', hour='0', minute='30',  second='20' , misfire_grace_time=None)
     logger.info('Start the Scheduling~')
     sched.start()
 
